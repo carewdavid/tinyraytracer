ef2bdeb9d55785e0a6b7ff95f2e74e478afcc387
David <carew124@gmail.com>
2019-01-28 14:31:30 -0800

    Add multiple spheres to scene

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use std::fs::File;
 use std::f32;
 
 #[derive(Copy, Clone)]
-struct Pixel(f32, f32, f32);
+struct Color(f32, f32, f32);
 
 
-impl Pixel {
+impl Color {
     fn as_bytes(self) -> [u8; 3] {
         [
             (255.0 * 0.0_f32.max(1.0_f32.min(self.0))) as u8,
@@ -20,11 +20,19 @@ impl Pixel {
 struct Point(f32, f32, f32);
 
 impl Point {
-    fn sub(&self, subtrahend: Point) -> Point {
+    fn sub(&self, subtrahend: &Point) -> Point {
         Point(
             self.0 - subtrahend.0,
             self.1 - subtrahend.1,
             self.2 - subtrahend.2
+            )
+    }
+
+    fn add(&self, addend: &Point) -> Point {
+        Point(
+            self.0 + addend.0,
+            self.1 + addend.1,
+            self.2 + addend.2
             )
     }
 
@@ -36,7 +44,7 @@ impl Point {
             )
     }
 
-    fn mult(&self, other: Point) -> f32 {
+    fn mult(&self, other: &Point) -> f32 {
         (self.0 * other.0) + (self.1 * other.1) + (self.2 * other.2)
     }
 
@@ -49,19 +57,26 @@ impl Point {
     }
 }
 
+#[derive(Copy, Clone)]
+struct Material {
+    diffuse_color: Color
+}
+
+
 struct Sphere {
     center: Point,
-    radius: f32
+    radius: f32,
+    material: Material,
 }
 
 impl Sphere {
-    fn ray_intersect(&self, orig: Point, dir: Point) -> bool {
+    fn ray_intersect(&self, orig: &Point, dir: &Point) -> (bool, f32) {
         //Nasty vector math
         let L = self.center.sub(orig);
         let tca = L.mult(dir);
-        let d2 = L.mult(L) - tca * tca;
+        let d2 = L.mult(&L) - tca * tca;
         if d2 > self.radius * self.radius  {
-            return false;
+            return (false, 0.0_f32)
         }
         let thc = (self.radius * self.radius - d2).sqrt();
         let mut t0 = tca - thc;
@@ -69,46 +84,63 @@ impl Sphere {
         if t0 < 0.0 {
             t0 = t1;
         }
-        if t0 < 0.0 {
+        let result = if t0 < 0.0 {
             false
         }else{
             true
-        }
+        };
+        (result, t0)
     }
 }
         
-fn cast_ray(origin: Point, dir: Point, sphere: &Sphere) -> Pixel {
-    if !sphere.ray_intersect(origin, dir) {
-        Pixel(0.2, 0.7, 0.8)
+fn scene_intersect(origin: &Point, dir: &Point, spheres: &Vec<Sphere>) -> (bool, Material) {
+    let mut spheres_dist =f32::MAX;
+    let mut material = Material{diffuse_color: Color(0.0, 0.0, 0.0)};
+    for sphere in spheres {
+        let (intersect, dist) = sphere.ray_intersect(origin, dir);
+        if intersect && dist < spheres_dist {
+            spheres_dist = dist;
+            //let hit = origin.add(&dir.mult_sca(dist));
+            material = sphere.material;
+        }
+    }
+    (spheres_dist < 1000.0, material)
+}
+
+
+fn cast_ray(origin: &Point, dir: &Point, spheres: &Vec<Sphere>) -> Color {
+    let (hit, material) = scene_intersect(origin, dir, spheres);
+    if !hit {
+        Color(0.2, 0.7, 0.8) //Backgorund color
     }else{
-        Pixel(0.4, 0.4, 0.3)
+        material.diffuse_color
     }
 }
 
 
-fn render(sphere: Sphere) {
+fn render(spheres: &Vec<Sphere>) {
     let width = 1024;
     let height = 768;
     let fov = f32::consts::FRAC_PI_2;
 
-    let mut framebuffer : Vec<Pixel> = Vec::new();
+    let mut framebuffer : Vec<Color> = Vec::new();
 
     for j in 0..height {
         for i in 0..width {
             let x = (2.0 * (i as f32 + 0.5) / (width as f32) - 1.0) * (fov / 2.0).tan() * width as f32 / (height as f32);
             let y = -(2.0 * (j as f32 + 0.5) / (height as f32) - 1.0) * (fov / 2.0).tan();
             let dir = Point(x, y, -1.0).normalize();
-            framebuffer.push(cast_ray(Point(0.0,0.0,0.0), dir, &sphere));
+            framebuffer.push(cast_ray(&Point(0.0,0.0,0.0), &dir, &spheres));
 
         }
     }
 
     let mut out = File::create("out.ppm").unwrap();
 
-    out.write_fmt(format_args!("P6\n{} {}\n255\n", width, height));
+    out.write_fmt(format_args!("P6\n{} {}\n255\n", width, height)).unwrap();
 
     for pixel in framebuffer.iter() {
-        out.write(&pixel.as_bytes());
+        out.write(&pixel.as_bytes()).unwrap();
     }
 }
 
@@ -116,6 +148,15 @@ fn render(sphere: Sphere) {
 
 
 fn main() {
-    let sphere: Sphere = Sphere{center: Point(-3.0, 0.0, -16.0), radius: 2.0};
-    render(sphere);
+    let ivory = Material{diffuse_color: Color(0.4, 0.4, 0.3)};
+    let red_rubber = Material{diffuse_color: Color(0.3, 0.1, 0.1)};
+
+    let spheres = vec![
+        Sphere{center: Point(-3.0, 0.0, -16.0), radius: 2.0, material: ivory},
+        Sphere{center: Point(-1.0, -1.5, -12.0), radius: 2.0, material: red_rubber},
+        Sphere{center: Point(1.5, -0.5, -18.0), radius: 2.0, material: red_rubber},
+        Sphere{center: Point(7.0, 5.0, -18.0), radius: 2.0, material: ivory},
+    ];
+
+    render(&spheres);
 }
